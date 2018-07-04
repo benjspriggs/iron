c8d1785fea1d053641459dce511481e7a977d89e
Benjamin Spriggs <ben@sprico.com>
2018-07-03 21:57:24 -0700

    Feat/create react app (#7)
    * Remove ampersand-cli
    
    * create-react-app
    
    * Re-add app.json for heroku deployment
    
    * Minimal working version using create-react-app and current versions of redux-thunk, etc
    
    * Remove import to unused composer

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { render } from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+import { ConnectedRouter } from 'connected-react-router';
+import store, { history } from './store';
+
+const target = document.getElementById('root');
+
+render(
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <div>
+        <App />
+      </div>
+    </ConnectedRouter>
+  </Provider>, 
+  target);
+registerServiceWorker();
