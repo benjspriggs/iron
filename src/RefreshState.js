a815f324ee34149776d3df124d2af99d9ed61ffd
bspriggs <ben@sprico.com>
2018-07-10 13:14:34 -0700

    Catch errors

--- src/RefreshState.js
+++ src/RefreshState.js
@@ -2,6 +2,7 @@ import React from 'react'
 
 const purgeState = persistor => () => persistor.purge()
   .then(() => alert('State has been purged'))
+  .catch(e => console.log(e))
 
 export default props => (
   <button onClick={purgeState(props.persistor)}>
