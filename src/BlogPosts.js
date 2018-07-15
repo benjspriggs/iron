0db440f9b3e16374708521f0d1da764d2b31b898
bspriggs <ben@sprico.com>
2018-07-14 21:53:29 -0700

    Add link in enhanced blog post

--- src/BlogPosts.js
+++ src/BlogPosts.js
@@ -1,15 +1,28 @@
 import React from 'react'
-import BlogPost from './BlogPost'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
-
+import { Link } from 'react-router-dom'
 import {
+  Button,
+  Icon,
   List
 } from 'semantic-ui-react'
 
+import BlogPost from './BlogPost'
+
+const EnhancedBlogPost = (post, id) => (
+<List.Item key={id}>
+	<BlogPost {...post} />
+    <Button icon labelPosition="left" as={Link} to={`/view/${post.id}`}>
+      <Icon name="eye" />
+    View
+    </Button>
+	</List.Item>
+)
+
 const BlogPosts = ({ posts }) => (
   <List relaxed divided>
-    { posts.map((p, idx) => <List.Item key={idx}><BlogPost {...p} /></List.Item>) }
+    { posts.map(EnhancedBlogPost) }
   </List>
 )
 
