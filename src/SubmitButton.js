f377e5d43f63d2c317c46841b9a5733e199ec8e2
bspriggs <ben@sprico.com>
2018-07-14 21:28:15 -0700

    Update implementation

--- src/SubmitButton.js
+++ src/SubmitButton.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 import { Button } from 'semantic-ui-react'
 
 const SubmitButton = ({
-  isSubmitting
+  isSubmitting = false
 }) => (
   <Button type="submit" disabled={isSubmitting}>
     Submit
