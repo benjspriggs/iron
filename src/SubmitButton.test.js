1ea53d058eed6a48b76b3fc671617c61bdd5a8b0
bspriggs <ben@sprico.com>
2018-07-14 21:28:09 -0700

    Add SubmitButton test

---
+++ src/SubmitButton.test.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+
+import SubmitButton from './SubmitButton'
+
+describe(SubmitButton.DisplayName, () => {
+	it('Has a single button', () => {
+		const mount = shallow(<SubmitButton />)
+		expect(mount.find('Button')).toHaveLength(1)
+	})
+
+	it('is enabled by default', () => {
+		const mount = shallow(<SubmitButton />)
+		expect(mount.find('Button').prop('disabled')).toEqual(false)
+	})
+
+	it('can be disabled', () => {
+		const mount = shallow(<SubmitButton isSubmitting={true}/>)
+		expect(mount.find('Button').prop('disabled')).toEqual(true)
+	})
+})
