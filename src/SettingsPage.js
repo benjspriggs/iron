e84445aadd2e981b36d147b6e0a462309a1167b0
bspriggs <ben@sprico.com>
2018-07-14 22:47:13 -0700

    Divide and flow content on the Settings page

--- src/SettingsPage.js
+++ src/SettingsPage.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Container, Header } from "semantic-ui-react"
+import { Container, Header, Divider } from "semantic-ui-react"
 
 import SimpleGithubRepoForm from "./SimpleGithubRepoForm"
 import GithubRepoForm from "./GithubRepoForm"
@@ -10,12 +10,18 @@ const SettingsPage = () => (
   <Container>
     <Header as="h2">Settings</Header>
     <Header as="h3">Form</Header>
+    <Header as="h4">Simple Form</Header>
     <SimpleGithubRepoForm />
+    <Divider />
+    <Header as="h4">Full Form</Header>
     <GithubRepoForm />
-    <Header as="h3">Errors</Header>
-    <GithubErrorsDisplay />
+    <Divider section />
     <Header as="h3">Repos</Header>
     <CurrentRepos />
+    <Divider section />
+    <Header as="h3">Errors</Header>
+    <GithubErrorsDisplay />
+    <Divider section />
   </Container>
 )
 
