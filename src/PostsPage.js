0d1c591350a32dd2664b751cdbca719a2ed31f9c
bspriggs <ben@sprico.com>
2018-07-14 22:32:22 -0700

    Remove lorem-ipsum dependency

--- src/PostsPage.js
+++ src/PostsPage.js
@@ -2,23 +2,11 @@ import React from "react"
 import BlogPosts from "./BlogPosts"
 import { Container } from "semantic-ui-react"
 import _ from "lodash"
-import lorem from "lorem-ipsum"
 
 import { connect } from "react-redux"
 
-const generatePosts = () =>
-  _.times(10, i => ({
-    title: "anything",
-    content: lorem({ count: 5, units: "paragraphs" }),
-    source: "me"
-  }))
-
 export default connect(state => state.posts)(props => (
   <Container>
-    <BlogPosts
-      posts={
-        props.posts && props.posts.length > 0 ? props.posts : generatePosts()
-      }
-    />
+    <BlogPosts posts={props.posts} />
   </Container>
 ))
