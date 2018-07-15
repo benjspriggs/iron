e99f3ecca6cfbbabb43bc752c2b848204468672d
bspriggs <ben@sprico.com>
2018-07-14 21:53:19 -0700

    Update implementation of BlogPost

--- src/BlogPost.js
+++ src/BlogPost.js
@@ -4,10 +4,7 @@ import renderHTML from 'react-render-html'
 import {
   Container,
   Header,
-  Button,
-  Icon
 } from 'semantic-ui-react'
-import { Link } from 'react-router-dom'
 
 export const BlogPost = ({ id, title, source, html = false, content = '', tags = [], date = '' }) => (
   <Container>
@@ -21,11 +18,6 @@ export const BlogPost = ({ id, title, source, html = false, content = '', tags =
     <Container text>
       {html ? renderHTML(content) : content}
     </Container>
-
-    <Button icon labelPosition="left" as={Link} to={`/view/${id}`}>
-      <Icon name="eye" />
-    View
-    </Button>
   </Container>
 )
 
