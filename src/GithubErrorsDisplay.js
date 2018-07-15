cef5c3dd2dadbf47561f23142f916edbe6339fb6
bspriggs <ben@sprico.com>
2018-07-14 20:45:58 -0700

    Add button to clear errors

--- src/GithubErrorsDisplay.js
+++ src/GithubErrorsDisplay.js
@@ -3,27 +3,40 @@ import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import _ from 'lodash'
 import {
+  Container,
   List,
+  Button,
   Message
 } from 'semantic-ui-react'
 
-const GithubErrorsDisplay = ({ githubErrors }) => {
+import { clearGithubErrors } from './dux/github'
+
+const GithubErrorsDisplay = ({ githubErrors, clearGithubErrors }) => {
   const extra = githubErrors ? _.flatten(Object.keys(githubErrors)
     .map(owner => Object.keys(githubErrors[owner])
       .reduce((rs, r) => [...rs,
         `${owner}/${r}: ${githubErrors[owner][r].error.message}`], []))) : []
 
   return (
-    <List>
-      { extra.map((e, key) => <Message key={key} content={e} header={'Error'}/>) }
-    </List>
+    <Container>
+      <List>
+        { extra.map((e, key) => <Message key={key} content={e} header={'Error'}/>) }
+      </List>
+      <Button onClick={() => clearGithubErrors()}>
+    Clear Errors
+      </Button>
+    </Container>
   )
 }
 
 GithubErrorsDisplay.propTypes = {
-  githubErrors: PropTypes.object
+  githubErrors: PropTypes.object,
+  clearGithubErrors: PropTypes.func.isRequired
 }
 
 export default connect(
-  state => ({ githubErrors: state.github.errors })
+  state => ({ githubErrors: state.github.errors }),
+  dispatch => ({
+    clearGithubErrors: () => dispatch(clearGithubErrors())
+  })
 )(GithubErrorsDisplay)
