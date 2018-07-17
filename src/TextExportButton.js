3a57d56dc2b6d344405331464829846f1afc5a8a
bspriggs <ben@sprico.com>
2018-07-17 13:16:20 -0700

    Add button for downloading all the posts

--- src/TextExportButton.js
+++ src/TextExportButton.js
@@ -2,15 +2,26 @@ import React from "react"
 import PropTypes from "prop-types"
 import { Button } from "semantic-ui-react"
 import Download from "@axetroy/react-download"
+import { connect } from "react-redux"
+
+const formatPostForTextExport = post =>
+  `${post.title} (by ${post.source})\n${
+    post.content ? post.content.join("\n") : ""
+  }\n\n`
 
 const TextExportButton = props => (
-  <Download file="text.txt" content={props.content}>
+  <Download
+    file="text.txt"
+    content={props.posts.map(formatPostForTextExport).join("")}
+  >
     <Button type="button">Click to download</Button>
   </Download>
 )
 
 TextExportButton.propTypes = {
-  content: PropTypes.string
+  content: PropTypes.array
 }
 
-export default TextExportButton
+export default connect(state => ({ posts: Object.values(state.posts.posts) }))(
+  TextExportButton
+)
