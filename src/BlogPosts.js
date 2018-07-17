62d1ca907d29cb39d195d2963c522482b44b3e64
bspriggs <ben@sprico.com>
2018-07-17 13:44:34 -0700

    Add source viewing button

--- src/BlogPosts.js
+++ src/BlogPosts.js
@@ -2,17 +2,29 @@ import React from "react"
 import PropTypes from "prop-types"
 import { connect } from "react-redux"
 import { Link } from "react-router-dom"
-import { Button, Icon, List } from "semantic-ui-react"
+import { Button, Icon, List, Divider } from "semantic-ui-react"
 
 import BlogPost from "./BlogPost"
 
 const EnhancedBlogPost = (post, id) => (
   <List.Item key={id}>
     <BlogPost {...post} />
+    <Divider hidden />
     <Button icon labelPosition="left" as={Link} to={`/view/${post.id}`}>
       <Icon name="eye" />
       View
     </Button>
+    {post.url ? (
+      <Button
+        attached="right"
+        href={post.url}
+        icon="external alternate"
+        labelPosition="left"
+        content="Source"
+      />
+    ) : (
+      ""
+    )}
   </List.Item>
 )
 
