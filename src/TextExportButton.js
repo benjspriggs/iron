f8ef06979dae9127af7567aa82dc00f0ec98b72e
bspriggs <ben@sprico.com>
2018-07-17 12:36:39 -0700

    Add proptypes

--- src/TextExportButton.js
+++ src/TextExportButton.js
@@ -1,4 +1,5 @@
 import React from "react"
+import PropTypes from "prop-types"
 import { Button } from "semantic-ui-react"
 import Download from "@axetroy/react-download"
 
@@ -7,5 +8,9 @@ const TextExportButton = props => (
     <Button type="button">Click to download</Button>
   </Download>
 )
+
+TextExportButton.propTypes = {
+  content: PropTypes.string
+}
 
 export default TextExportButton
