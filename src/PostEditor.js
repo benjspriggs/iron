17080683decc3d4b4dbd17c52b0b047672b075b1
bspriggs <ben@sprico.com>
2018-08-06 13:06:27 -0700

    Add formik wrapper to posteditor for validation

--- src/PostEditor.js
+++ src/PostEditor.js
@@ -1,46 +1,58 @@
 import React from "react"
 import PropTypes from "prop-types"
 import Editor from "rich-markdown-editor"
-import { Container, Button, Input, Segment } from "semantic-ui-react"
+import { Form, Container, Button, Input, Segment } from "semantic-ui-react"
 import BlogPost from "./BlogPost"
 import _ from "lodash"
 import { DateInput } from "semantic-ui-calendar-react"
+import { Formik } from "formik"
 
 import "semantic-ui-calendar-react/dist/css/calendar.min.css"
 
 const PostEditor = props => (
-  <Container>
-    <Container>
-      <Input
-        required
-        placeholder="Title"
-        value={props.post.title}
-        onChange={(e, input) => props.updateTitle(input.value)}
-      />
-      <Input
-        required
-        placeholder="Author"
-        value={props.post.source}
-        onChange={(e, input) => props.updateSource(input.value)}
-      />
-      <Segment>
-        <Editor
-          id="content"
-          defaultValue={_.join(props.post.content, "\n")}
-          onChange={content => props.updateContent(content.split("\n"))}
+  <Formik
+    render={() => (
+      <Form>
+        <Form.Field>
+          <Input
+            required
+            placeholder="Title"
+            value={props.post.title}
+            onChange={(e, input) => props.updateTitle(input.value)}
+          />
+        </Form.Field>
+        <Form.Field>
+          <Input
+            required
+            placeholder="Author"
+            value={props.post.source}
+            onChange={(e, input) => props.updateSource(input.value)}
+          />
+        </Form.Field>
+        <Form.Field>
+          <Segment>
+            <Editor
+              id="content"
+              defaultValue={_.join(props.post.content, "\n")}
+              onChange={content => props.updateContent(content.split("\n"))}
+            />
+          </Segment>
+        </Form.Field>
+        <DateInput
+          name="date"
+          placeholder="Date"
+          value={props.post.date ? props.post.date : ""}
+          onChange={(e, i) => props.updateDate(i.value)}
         />
-      </Segment>
-      <DateInput
-        name="date"
-        placeholder="Date"
-        value={props.post.date ? props.post.date : ""}
-        onChange={(e, i) => props.updateDate(i.value)}
-      />
-    </Container>
-    <Button onClick={() => props.handlePostUpdate(props.post)}>
-      {props.buttonText}
-    </Button>
-  </Container>
+        <Button
+          type="submit"
+          onClick={() => props.handlePostUpdate(props.post)}
+        >
+          {props.buttonText}
+        </Button>
+      </Form>
+    )}
+  />
 )
 
 PostEditor.propTypes = {
